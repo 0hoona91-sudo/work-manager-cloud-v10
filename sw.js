090e1a58c58b28ff794a7968755d58c740da6a71
0hoona91-sudo <0.hoona91@gmail.com>
2026-09-11 22:40:37 +0900

    fix: harden rolling repeat replenishment

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "work-manager-v10-shell-2026-09-10-25";
+const CACHE_NAME = "work-manager-v10-shell-2026-09-11-26";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./js/cloud-sync.js?v=20260910-25",
+  "./js/cloud-sync.js?v=20260911-26",
   "./js/firebase-config.js?v=20260905-1",
   "./icons/app-icon-192.png",
   "./icons/app-icon-512.png",
